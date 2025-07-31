25f69c921db2509e0d0813610ac8b92ed2e85144
Saif <ro1c@yahoo.com>
2025-07-31 23:22:41 +0300

    feat: implement overlay functionality for mobile navigation

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burgerBtn = document.getElementById('burgerBtn');
     const navBar = document.querySelector('.navBar');
-    
+    const overlay = document.getElementById('overlay');
+
     burgerBtn.addEventListener('click', function() {
         navBar.classList.toggle('show');
+        overlay.classList.toggle('show');
         burgerBtn.classList.toggle('active');
     });
 
+    const removeClasses = () => {
+        navBar.classList.remove('show');
+        overlay.classList.remove('show');
+        burgerBtn.classList.remove('active');
+    }
     const navLinks = navBar.querySelectorAll('a');
     navLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            navBar.classList.remove('show');
-            burgerBtn.classList.remove('active');
-        });
+        link.addEventListener('click', removeClasses);
     });
+
+    overlay.addEventListener('click', removeClasses);
 });
